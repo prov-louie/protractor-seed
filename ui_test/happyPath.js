1e1189e3f0aab51bec5184bf4f9450b88be3a7f1
prov-louie <louis.perez@providence.org>
2016-02-18 08:58:08 -0800

    Add first page object methods

--- ui_test/happyPath.js
+++ ui_test/happyPath.js
@@ -1,17 +1,31 @@
+"use strict";
+
 describe('angularjs homepage todo list', function() {
   it('should add a todo', function() {
-    browser.get('https://angularjs.org');
+    let todoPage = new TodoPage()
+    todoPage.go()
+    todoPage.addTodo('write first protractor test')
 
-    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-    element(by.css('[value="add"]')).click();
-
-    var todoList = element.all(by.repeater('todo in todoList.todos'));
-    expect(todoList.count()).toEqual(3);
-    expect(todoList.get(2).getText()).toEqual('write first protractor test');
+    var todoList = element.all(by.css('.checkbox'))
+    expect(todoList.count()).toEqual(3)
+    expect(todoList.get(2).getText()).toEqual('write first protractor test')
 
     // You wrote your first test, cross it off the list
-    todoList.get(2).element(by.css('input')).click();
-    var completedAmount = element.all(by.css('.done-true'));
-    expect(completedAmount.count()).toEqual(2);
-  });
-});
+    todoList.get(2).element(by.css('input')).click()
+    var completedAmount = element.all(by.css('.done-true'))
+    expect(completedAmount.count()).toEqual(2)
+  })
+})
+
+class TodoPage {
+  constructor () {}
+
+  go () {
+    browser.get('https://angularjs.org')
+  }
+
+  addTodo (todoString) {
+    element(by.model('todoList.todoText')).sendKeys(todoString);
+    element(by.css('[value="add"]')).click();
+  }
+}
